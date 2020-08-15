27a79a74887e42f1bb639ccc0e9f24b3b1d27022
Sherif Elmetwally <sge5@njit.edu>
2020-08-15 14:02:28 -0400

    make mobile friendly

--- porto4/src/components/work/FilterTabs.js
+++ porto4/src/components/work/FilterTabs.js
@@ -5,7 +5,8 @@ import '../../styles/work/filter-tabs.css'
 export default class FilterTabs extends React.Component {
 
     state = {
-        projects: []
+        projects: [],
+        width: window.innerWidth,
     }
     
     componentDidMount() {
@@ -15,24 +16,50 @@ export default class FilterTabs extends React.Component {
     }
 
     render() {
-        return (
-            <>
-                <h1 className="selected-work">Selected Work 👇</h1>
-                <div className="filter-wrappers">
-                    <div className="filter-tab active-filter-tab first-tab" onClick={e => this.props.reset(e)}>
-                        All ({this.props.projects.length})
+        const { width } = this.state;
+        const isMobile = width <= 600;
+
+        if (!isMobile) {
+            return (
+                <>
+                    <h1 className="selected-work">Selected Work 👇</h1>
+                    <div className="filter-wrappers">
+                        <div className="filter-tab active-filter-tab first-tab" onClick={e => this.props.reset(e)}>
+                            All ({this.props.projects.length})
+                        </div>
+                        <div className="filter-tab second-tab" onClick={e => this.props.filterWebDev(e)}>
+                            Web Development ({this.props.projects.filter(project => project.type == 'web development').length})
+                        </div>
+                        <div className="filter-tab third-tab" onClick={e => this.props.filterDesigns(e)}>
+                            Design ({this.props.projects.filter(project => project.type == 'design').length})
+                        </div>
+                        <div className="filter-tab last-tab" onClick={e => this.props.filterDataViz(e)}>
+                            Data Visualization ({this.props.projects.filter(project => project.type == 'data viz').length})
+                        </div>
                     </div>
-                    <div className="filter-tab second-tab" onClick={e => this.props.filterWebDev(e)}>
-                        Web Development ({this.props.projects.filter(project => project.type == 'web development').length})
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <h1 className="selected-work">Selected Work 👇</h1>
+                    <div className="filter-wrappers">
+                        <div className="filter-tab active-filter-tab first-tab" onClick={e => this.props.reset(e)}>
+                            All ({this.props.projects.length})
+                        </div>
+                        <div className="filter-tab second-tab" onClick={e => this.props.filterWebDev(e)}>
+                            Web Dev
+                        </div>
+                        <div className="filter-tab third-tab" onClick={e => this.props.filterDesigns(e)}>
+                            Design
+                        </div>
+                        <div className="filter-tab last-tab" onClick={e => this.props.filterDataViz(e)}>
+                            Data Viz
+                        </div>
                     </div>
-                    <div className="filter-tab third-tab" onClick={e => this.props.filterDesigns(e)}>
-                        Design ({this.props.projects.filter(project => project.type == 'design').length})
-                    </div>
-                    <div className="filter-tab last-tab" onClick={e => this.props.filterDataViz(e)}>
-                        Data Visualization ({this.props.projects.filter(project => project.type == 'data viz').length})
-                    </div>
-                </div>
-            </>
-        )
+                </>
+            )
+        }
+
     }
 }
